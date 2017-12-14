81f673e67a1ae97db780958e0412d43c7435aa81
jia27222 <34232159+jia27222@users.noreply.github.com>
2017-12-14 14:36:07 +0800

    Update function.php

--- function.php
+++ function.php
@@ -3,14 +3,13 @@
 
     /**
      * 处理文章内容的图片src属性
-     * @param unknown $str
+     * @param string $str 微信文章源码内容
      */
     public function imgsrc($str){
         $APP_URL = C('APP_HTTPURL');//项目访问地址
         //处理微信图片无法正常显示问题
         $msg = preg_replace("/src=\"https:\/\/mmbiz\./", " data-src=\"".$APP_URL."index.php/ArticleIndex/pic/https://mmbiz.", $str);
         $msg = preg_replace("/src=\"http:\/\/mmbiz\./", " data-src=\"".$APP_URL."index.php/ArticleIndex/pic/http://mmbiz.", $msg);
-        //tp=webp&
         $msg = preg_replace("/tp=webp\&/", "", $msg);
         
         //视频不加载
@@ -18,24 +17,20 @@
         $msg = preg_replace("/a\.style\.display = \"none\";/", " ", $msg);
         $msg = preg_replace("/a.setAttribute\(\"data-src\"/", "a.setAttribute(\"src\"", $msg);
         $msg = preg_replace("/mydiv\.style\.cssText = \"/", "mydiv.style.cssText = \"display: none;", $msg);
-        //style="display: none;
         
         // 图片延迟加载
         //$msg = preg_replace("/\ssrc/", " data-src", $msg);
         return $msg;
     }
 
-/**
-     * 显示微信图片
-     微信图片地址样例
+    /**
+     *显示微信图片方法
+     *微信图片地址样例
      *$imgurl = 'http://mmbiz.qpic.cn/mmbiz_jpg/mxaa4wWaSsLTJP7sc76y6wyu8AFPV1XfgM8IXHLiaZmZbtr3XpicE0dOGUek6lh0HaO3yGABNz5lL00bmGFd8plQ/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1'
      */
     public function pic(){
-//         print_r($_GET);die();
         $url =  $_SERVER["REQUEST_URI"]; 
-//         echo $url;
         $imgurl = substr($url, 28);
-//         echo $imgurl;
         $imgtype = $_GET['wx_fmt'];
         
         header( "Content-type: image/{$imgtype}");
